bf3bc888525f77378261733267d627f743065507
diegovieiradv <dvdiego87@gmail.com>
2025-07-05 02:28:42 -0300

    atualizei project
    atualizei projeto

--- script.js
+++ script.js
@@ -1,16 +1,32 @@
-function criarAnimacao() {
+let webhook = "https://rodolfomori123123.app.n8n.cloud/webhook/animacao-css";
 
-  let textInput = document.querySelector(".input-animation").value;
-  let buttonMagic = document.querySelector(".button-magic");
-  buttonMagic.disabled = true;
-  buttonMagic.innerHTML = "Criando...";
+// funcao assincrona
+async function cliqueiNoBotao() {
+  let textoInput = document.querySelector(".input-animacao").value;
+  let codigo = document.querySelector(".codigo-css");
+  let areaResultado = document.querySelector(".area-resultado");
 
-  let codigoCSS = document.querySelector(".codigo-css");
-  codigoCSS.innerHTML = "// Aqui aparecerá o código da animação";
-  
-  let resultado = document.querySelector(".resultado");
-  resultado.innerHTML = "A mágica aparecerá aqui!";
-  setTimeout(() => {
-    buttonMagic.disabled = false;
-  }, 3000);
+  // fetch - 1) O endereco 2) configuracoes 3) os dados
+  // JSON - O formato de dados que usamos na internet
+
+  let resposta = await fetch(webhook, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ pergunta: textoInput }),
+  });
+
+  let resultado = await resposta.json();
+
+  let info = JSON.parse(resultado.resposta);
+
+  console.log(info);
+
+  codigo.innerHTML = info.code;
+
+  areaResultado.innerHTML = info.preview;
+
+  document.head.insertAdjacentHTML(
+    "beforeend",
+    "<style>" + info.style + "</style>"
+  );
 }
